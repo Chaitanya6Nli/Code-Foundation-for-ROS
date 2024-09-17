153a6fc87637ef725ec33c3a3a43e824bdf540d9
Chaitanya Belekar <157277524+Chaitanya6Nli@users.noreply.github.com>
2024-09-17 14:28:28 +0530

    Update task1.sh

--- Prerequisites Exam/Linux_exam/task1.sh
+++ Prerequisites Exam/Linux_exam/task1.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Move inside the linux_exam folder
+cd ~/catkin_ws/src/linux_exam
+
+# Create folder structure this->is->my->linux->exam
+mkdir -p this/is/my/linux/exam
+
+# Create the file my_file.py inside the exam folder
+touch this/is/my/linux/exam/my_file.py
+
+# Print the completion message
+echo "This bash script has finished!"
 
